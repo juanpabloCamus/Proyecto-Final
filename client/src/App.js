26a3f26c09368aaca46110f8a1355b130f942380
Juan Daniel Sanchez Orozco <juandaniel.25@outlook.com>
2022-05-24 12:36:22 -0500

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { LandinPage } from "./components/landin_page/LandinPage";
+
+
+
 function App() {
-  const { 
-    loginWithPopup, 
-    loginWithRedirect,
-    logout, 
-    user, 
-    isAuthenticated 
-  } =useAuth0();
+  
 
   return <div className="App">
-    <h1>Auth0 authentication</h1>
-    <ul>
-      <li><button onClick={loginWithPopup}>Login with Popup</button></li>
-      <li><button onClick={loginWithRedirect}>Login with Redirect</button></li>
-      <li><button onClick={logout}>Logout</button></li>
-
-    </ul>
-    <h5>User id {isAuthenticated ? "Logged in" : "Not logged in"}</h5>
-    {isAuthenticated && (
-  <pre style={{textAlign: 'start'}}>{JSON.stringify(user,null,2)}</pre>
- )} </div>;
+      <LandinPage/>
+    </div>;
 }
 
 export default App;
