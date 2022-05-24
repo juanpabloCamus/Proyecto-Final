1f1526e2a067fb597d669088a2394f7246d561d7
Agze Gothicrow <agusezeq@gmail.com>
2022-05-24 19:07:36 -0300

    ruta POST para registro de usuarios

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -13,5 +13,49 @@ router.get('/', async (req,res)=>{
     }
 })
 
+router.post('/', async (req,res)=>{
+    try{
+        const {name, last_name, email, password} = req.body
+
+        if(!name||!last_name||!email||!password){
+            res.send('Hay un campo invalido.')
+        }else{
+            if(!/^[a-zA-Z]+$/.test(name)){
+                res.send('El nombre solo admite letras')
+            }else if(!/^[a-zA-Z]+$/.test(last_name)){
+                res.send('El apellido solo admite letras')
+            }else if(!/^[a-zA-Z0-9_\-\.]+@+[a-zA-Z]+.com/.test(email)){
+                res.send('El mail tiene un formato invalido')
+            }else{
+                let mailUser = await user_account.findAll({
+                    where:{
+                        email: email
+                    }
+                })
+                let mailCompany = await company_account.findAll({
+                    where:{
+                        email: email
+                    }
+                })
+                console.log(mailUser)
+                console.log(mailCompany)
+                if(mailUser.length<1&&mailCompany.length<1){
+                    const newUser = await user_account.create({
+                        name,
+                        last_name,
+                        email,
+                        password
+                    })
+                    res.send('Usuario creado correctamente.')
+                }else{
+                    res.send('El email ya se encuentra registrado.')
+                }
+            }
+        }
+
+    }catch(error){
+        console.log(error)
+    }
+})
 
 module.exports = router;
