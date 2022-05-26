7c9ef23121814ce35ba9eeae978dbf3acc46dcdf
Juan Pablo <juanpablocamus28@gmail.com>
2022-05-26 18:12:12 -0300

    prepush a dani

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Routes,Route } from 'react-router-dom'
-import Home from '../components/Home/Home'
+import Home from '../Components/Home/Home'
 
 
 export const AppRouter = () => {
   return (
     <div>
-   
+  
           <Routes>
             <Route path='/'element={<Home/>} /> 
 
--- client/src/routes/LandinPageRoute.jsx
+++ client/src/routes/LandinPageRoute.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import { BrowserRouter,Routes, Route } from 'react-router-dom'
- import { LandinPage } from '../components/landin_page/LandinPage'
+import { LandinPage } from '../Components/landin_page/LandinPage'
 import { AppRouter } from './AppRouter'
 
 export const LandinPageRoute = () => {
   return (
     <div>
-
         <BrowserRouter>
             <Routes>
                 <Route path='/' exact  element={ <LandinPage /> } /> 
                 <Route path='/home/' element={<AppRouter/>} />
-
             </Routes>
         </BrowserRouter>
         
