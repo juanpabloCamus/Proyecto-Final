a8f29e294843176d7cb15b888f122b0f0e36973f
PalomaV <palomavaira@hotmail.com>
2022-06-02 17:50:13 -0300

    formulario edit Jobs

--- client/src/components/Home/Company/PostJobOfferDetail/EditJobOffer/EditJobOffer.jsx
+++ client/src/components/Home/Company/PostJobOfferDetail/EditJobOffer/EditJobOffer.jsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 import React from "react";
-
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate, useParams } from "react-router";
 import { useForm } from "../../../../../hooks/useForm";
 import { modalActions } from "../../../../../redux/modal_slice/modalSlice";
-
+import Swal from 'sweetalert2'
 import "./edit.css";
 
 export const EditJobOffer = () => {
@@ -39,14 +38,45 @@ export const EditJobOffer = () => {
   const handleCloseModal = () => {
     dispatch(modalActions.setModalValue());
   };
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    editOffer();
+    dispatch(modalActions.setModalValue())
+  }
   const editOffer = async (id) => {
     try {
       const res = await axios.put(`http://localhost:3001/jobs/${id}`, formValues);
+      if(res.data.active === true){
+        Swal.fire({
+          icon: 'success',
+          text: ""
+        })
+      }else{
+        Swal.fire({
+          icon: 'error',
+          text: res.data
+        })
+      }
     } catch (error) {
 			console.log(error);
 		}
   };
 
-  return <></>;
+  
+  return (
+    <div>
+          <form onSubmit={handleSubmit}>
+          <label>Position</label>
+          <label></label>
+          <label></label>
+          <label></label>
+          <label></label>
+          <label></label>
+          <label></label>
+          <label></label>
+          </form>
+
+
+    </div>
+  )
 };
