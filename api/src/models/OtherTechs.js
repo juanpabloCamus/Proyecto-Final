7917fb34c6b3f64124f641ff83f98fdf37749689
Agze Gothicrow <agusezeq@gmail.com>
2022-06-05 16:24:34 -0300

    ahora es posible agregar nuevas techs

--- api/src/models/OtherTechs.js
+++ api/src/models/OtherTechs.js
@@ -10,7 +10,7 @@ module.exports = (sequelize) => {
         count:{
             type: DataTypes.INTEGER,
             allowNull: false,
-            defaultValue: 0
+            defaultValue: 1
         }
     },{timestamps:false})
 }
