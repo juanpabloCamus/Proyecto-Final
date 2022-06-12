de9e535ea448caa54df0da5cebb39521fad703bf
Agze Gothicrow <agusezeq@gmail.com>
2022-06-12 17:33:21 -0300

    reports y css q metio Joan

--- client/src/components/Home/User/Post/PostDetail/PostDeatail.jsx
+++ client/src/components/Home/User/Post/PostDetail/PostDeatail.jsx
@@ -27,7 +27,6 @@ function PostDetail() {
   const userLocalStorage = JSON.parse(localStorage.getItem("userData"));
 
   let idUser = userLocalStorage.id;
-  let profileType = userLocalStorage.profileType[0];
   let idJob = id;
 
   useEffect(() => {
@@ -43,15 +42,16 @@ function PostDetail() {
           'inappropiate lenguaje': 'Inappropiate Lenguaje',
           'false information': 'False Information',
           'inappropriate content':'Inappropriate Content'
-
-           },
+          },
        inputPlaceholder: 'Select reports',
        showCancelButton: true,
 
      })
 
+     let report = res.value
+
       if (res.isConfirmed) {
-        await axios.put(`jobs/report/${id}`, res.value, idUser, profileType);
+        await axios.put(`jobs/report/${id}`, report);
       }
     } catch (error) {
       console.log(error);
