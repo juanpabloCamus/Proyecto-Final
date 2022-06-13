231e689e28bd1591363e9dc4abe011cc79a5742d
Agze Gothicrow <agusezeq@gmail.com>
2022-06-12 22:52:31 -0300

    posible correccion de error en notis

--- client/src/components/notifications/notification_com_card/NotificationComCard.jsx
+++ client/src/components/notifications/notification_com_card/NotificationComCard.jsx
@@ -13,50 +13,78 @@ let dateOfSend = new Date(createdAt).toDateString().split(" ").slice(1, 4).join(
 
   return (
   <div className={styles.notification_card}>
-        <div>
-          <RiDeleteBinFill className={styles.delete_notification} title="Delete"/>
-        </div> 
-    {
-      codeNoti === 1 && (
+      <div>
+        <RiDeleteBinFill className={styles.delete_notification} title="Delete"/>
+      </div> 
+      {jobPosition ?
         <>
-          <div className={styles.notification_text}>
-                <p >From: {fullName}</p>
-                <p>Email: {emailUser}</p>
-                <p>Sent: {dateOfSend}</p>
-            </div>
-            <hr />
-            <p className={styles.notification_message}>Developer had accepted arrange an interview via Jitsi for the job position of: {jobPosition}</p>
-            <p>Scheduled meeting at: {dateTime}</p>
-              <br />
-            <div className={styles.notification_buttons}>
-                <button className={styles.notification_accept_button} onClick={() => navigate(`/company/meet/${id}`)}>Go to Jitsi</button>
-            </div>
+          {
+            codeNoti === 1 && (
+              <>
+                <div className={styles.notification_text}>
+                      <p >From: {fullName}</p>
+                      <p>Email: {emailUser}</p>
+                      <p>Sent: {dateOfSend}</p>
+                  </div>
+                  <hr />
+                  <p className={styles.notification_message}>Developer had accepted arrange an interview via Jitsi for the job position of: {jobPosition}</p>
+                  <p>Scheduled meeting at: {dateTime}</p>
+                    <br />
+                  <div className={styles.notification_buttons}>
+                      <button className={styles.notification_accept_button} onClick={() => navigate(`/company/meet/${id}`)}>Go to Jitsi</button>
+                  </div>
+            </>
+            )
+          }
+          {
+            codeNoti === 2 && (
+              <>
+                <div className={styles.notification_text}>
+                      <p >From: {fullName}</p>
+                      <p>Email: {emailUser}</p>
+                      <p>Sent: {dateOfSend}</p>
+                  </div>
+                  <hr />
+                  <p className={styles.notification_message}>{emailUser} had declined the meeting sent for the job position {jobPosition}</p>
+            </>
+            )
+          }
       </>
-      )
-      
-    }
-
-    {
-      codeNoti === 2 && (
+          :
         <>
-          <div className={styles.notification_text}>
-                <p >From: {fullName}</p>
-                <p>Email: {emailUser}</p>
-                <p>Sent: {dateOfSend}</p>
-            </div>
-            <hr />
-            <p className={styles.notification_message}>{emailUser} had declined the meeting sent for the job position {jobPosition}</p>
-
-            {/* <div className={styles.notification_buttons}>
-                <button className={styles.notification_accept_button}>Send Link</button>
-            </div> */}
-      </>
-      )
-      
-    }
-    
-      
-        
+        {
+            codeNoti === 1 && (
+              <>
+                <div className={styles.notification_text}>
+                      <p >From: {fullName}</p>
+                      <p>Email: {emailUser}</p>
+                      <p>Sent: {dateOfSend}</p>
+                  </div>
+                  <hr />
+                  <p className={styles.notification_message}>Developer had accepted arrange an interview via Jitsi.</p>
+                  <p>Scheduled meeting at: {dateTime}</p>
+                    <br />
+                  <div className={styles.notification_buttons}>
+                      <button className={styles.notification_accept_button} onClick={() => navigate(`/company/meet/${id}`)}>Go to Jitsi</button>
+                  </div>
+            </>
+            )
+          }
+          {
+            codeNoti === 2 && (
+              <>
+                <div className={styles.notification_text}>
+                      <p >From: {fullName}</p>
+                      <p>Email: {emailUser}</p>
+                      <p>Sent: {dateOfSend}</p>
+                  </div>
+                  <hr />
+                  <p className={styles.notification_message}>{fullName} had declined the meeting</p>
+            </>
+            )
+          }
+        </>
+      }
     </div>
   )
 }
