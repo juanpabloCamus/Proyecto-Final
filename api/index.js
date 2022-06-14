4cdb07060bbad7ff02728e213ea3567b6d0674d9
Gothicrow <97924806+Gothicrow@users.noreply.github.com>
2022-06-14 20:44:25 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const { loadDb } = require('./src/db')
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
-  db.sync({ force: false}).then(() => {
+  db.sync({ force: true}).then(() => {
     console.log('Modelos sincronizados');
   })
   .then(async ()=>{await loadDb()})
